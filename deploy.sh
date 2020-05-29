ccdada3ea4abaa42b8f26ae16294341572b624f3
zhaocheng <zhaocheng@westwin.com>
2020-05-29 17:38:34 +0800

    add

---
+++ deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+containerId="` sudo docker ps | grep "8081->80" | awk  '{print $1}' `"
+echo "containerId:$containerId"
+if [ -n "$containerId" ]
+then
+	sudo docker stop $containerId
+	sudo docker rm $containerId
+fi
+
+imageId="`sudo docker images | grep "api          v1.0" | awk  '{print $3}'`"
+echo "imageId:$imageId"
+if [ -n "$imageId" ]
+then
+	sudo docker rmi  $imageId
+fi
+
+sudo docker pull 542153354/api:v1.0 
+sudo docker run -d -p 8081:80 542153354/api:v1.0 /bin/sh 
+
+exit
